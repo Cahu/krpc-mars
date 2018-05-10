fd50b4263918518755088a3d7ae1b8f93ffae516
cahu <Cahu@users.noreply.github.com>
2018-05-10 13:13:09 +0200

    add impls for more types

--- src/codec.rs
+++ src/codec.rs
@@ -1,7 +1,11 @@
 use krpc;
 use protobuf;
+use protobuf::Message;
 
 use std::io::Read;
+use std::hash::Hash;
+use std::collections::HashSet;
+use std::collections::HashMap;
 
 
 #[derive(Debug, Fail)]
@@ -42,6 +46,91 @@ impl RPCExtractable for f64 {
 impl RPCExtractable for f32 {
     fn extract_value(input: &mut protobuf::CodedInputStream) -> Result<Self, protobuf::ProtobufError> {
         input.read_float()
+    }
+}
+
+impl RPCExtractable for u64 {
+    fn extract_value(input: &mut protobuf::CodedInputStream) -> Result<Self, protobuf::ProtobufError> {
+        input.read_uint64()
+    }
+}
+
+impl RPCExtractable for u32 {
+    fn extract_value(input: &mut protobuf::CodedInputStream) -> Result<Self, protobuf::ProtobufError> {
+        input.read_uint32()
+    }
+}
+
+impl RPCExtractable for i64 {
+    fn extract_value(input: &mut protobuf::CodedInputStream) -> Result<Self, protobuf::ProtobufError> {
+        input.read_sint64()
+    }
+}
+
+impl RPCExtractable for i32 {
+    fn extract_value(input: &mut protobuf::CodedInputStream) -> Result<Self, protobuf::ProtobufError> {
+        input.read_sint32()
+    }
+}
+
+impl RPCExtractable for String {
+    fn extract_value(input: &mut protobuf::CodedInputStream) -> Result<Self, protobuf::ProtobufError> {
+        input.read_string()
+    }
+}
+
+impl<T> RPCExtractable for Vec<T>
+    where T: RPCExtractable
+{
+    fn extract_value(input: &mut protobuf::CodedInputStream) -> Result<Self, protobuf::ProtobufError> {
+        let mut m = krpc::List::new();
+        m.merge_from(input)?;
+
+        let mut v = Vec::with_capacity(m.items.len());
+        for item in &m.items {
+            let mut i = protobuf::CodedInputStream::from_bytes(&item);
+            v.push(RPCExtractable::extract_value(&mut i)?);
+        }
+
+        Ok(v)
+    }
+}
+
+impl<T> RPCExtractable for HashSet<T>
+    where T: RPCExtractable + Hash + Eq,
+{
+    fn extract_value(input: &mut protobuf::CodedInputStream) -> Result<Self, protobuf::ProtobufError> {
+        let mut m = krpc::Set::new();
+        m.merge_from(input)?;
+
+        let mut s = HashSet::with_capacity(m.items.len());
+        for item in &m.items {
+            let mut i = protobuf::CodedInputStream::from_bytes(&item);
+            s.insert(RPCExtractable::extract_value(&mut i)?);
+        }
+
+        Ok(s)
+    }
+}
+
+impl<T, U> RPCExtractable for HashMap<T, U>
+    where T: RPCExtractable + Hash + Eq,
+          U: RPCExtractable
+{
+    fn extract_value(input: &mut protobuf::CodedInputStream) -> Result<Self, protobuf::ProtobufError> {
+        let mut m = krpc::Dictionary::new();
+        m.merge_from(input)?;
+
+        let mut h = HashMap::with_capacity(m.entries.len());
+        for entry in &m.entries {
+            let mut i_k = protobuf::CodedInputStream::from_bytes(&entry.key);
+            let mut i_v = protobuf::CodedInputStream::from_bytes(&entry.value);
+            let key = RPCExtractable::extract_value(&mut i_k)?;
+            let val = RPCExtractable::extract_value(&mut i_v)?;
+            h.insert(key, val);
+        }
+
+        Ok(h)
     }
 }
 
@@ -114,6 +203,55 @@ impl RPCEncodable for f64 {
 impl RPCEncodable for f32 {
     fn encode(&self, output: &mut protobuf::CodedOutputStream) -> Result<(), protobuf::ProtobufError> {
         output.write_float_no_tag(*self)
+    }
+}
+
+impl RPCEncodable for u32 {
+    fn encode(&self, output: &mut protobuf::CodedOutputStream) -> Result<(), protobuf::ProtobufError> {
+        output.write_uint32_no_tag(*self)
+    }
+}
+
+impl RPCEncodable for i32 {
+    fn encode(&self, output: &mut protobuf::CodedOutputStream) -> Result<(), protobuf::ProtobufError> {
+        output.write_sint32_no_tag(*self)
+    }
+}
+
+impl RPCEncodable for u64 {
+    fn encode(&self, output: &mut protobuf::CodedOutputStream) -> Result<(), protobuf::ProtobufError> {
+        output.write_uint64_no_tag(*self)
+    }
+}
+
+impl RPCEncodable for i64 {
+    fn encode(&self, output: &mut protobuf::CodedOutputStream) -> Result<(), protobuf::ProtobufError> {
+        output.write_sint64_no_tag(*self)
+    }
+}
+
+impl RPCEncodable for String {
+    fn encode(&self, output: &mut protobuf::CodedOutputStream) -> Result<(), protobuf::ProtobufError> {
+        output.write_string_no_tag(self)
+    }
+}
+
+impl<T> RPCEncodable for Vec<T>
+    where T: RPCEncodable
+{
+    fn encode(&self, output: &mut protobuf::CodedOutputStream) -> Result<(), protobuf::ProtobufError> {
+        let mut v = protobuf::RepeatedField::<Vec<u8>>::new();
+        for e in self {
+            v.push(e.encode_to_bytes()?);
+        }
+
+        let mut l = krpc::List::new();
+        l.set_items(v);
+
+        l.write_to(output)?;
+        output.flush()?;
+
+        Ok(())
     }
 }
 
