7ba06bbc84d8fbf3db9c5db50480ed2c9382fd96
cahu <cahu@users.noreply.github.com>
2018-05-11 15:09:58 +0200

    cargo:rerun-if-changed in build script

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 extern crate protoc_rust;
 
 fn main() {
+    println!("cargo:rerun-if-changed=protos/krpc.proto");
+    println!("cargo:rerun-if-changed=src/krpc.rs");
     protoc_rust::run(protoc_rust::Args {
         out_dir:   "src/",
         input:     &["protos/krpc.proto"],
